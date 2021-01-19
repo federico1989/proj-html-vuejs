c06276eefb65d29ee1aaaeda9e8fb232f158e85f
Federico <federico.scarpati1989@gmail.com>
2021-01-19 22:57:21 +0100

    Suddivisione sessioni e impostazione header
    Varie impostazioni

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
 let app = new Vue({
   el: "#root",
   data: {
+    logo: "assets/img/logo.png",
+    contacts: [
+      {
+        phone: [
+          font: "fas fa-phone-alt",
+          text: "+1(305) 1234-5678",
+
+        ],
+        email: [
+          font: "fas fa-envelope",
+          text: "hello@example.com",
+
+        ],
+        facebook: [
+          font: "fab fa-facebook-f",
+          text: "",
+
+        ],
+        twitter: [
+          font: "fab fa-twitter",
+          text: "",
+
+        ],
+        linkedin: [
+          font: "fab fa-linkedin-in",
+          text: "",
+        ]
+      }
+    ]
 
   },
   methods: {
